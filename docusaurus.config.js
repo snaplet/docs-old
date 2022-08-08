b5a5b636d9bc9edaeec77cde57fd2f429cb30c28
khaya-zulu <kzulu321@gmail.com>
2022-08-08 16:10:10 +0200

    enable search

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,75 +1,80 @@
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
-  title: 'Snaplet',
-  tagline: 'Work with your database as easily as your code',
-  url: 'https://docs.snaplet.dev',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.png',
-  organizationName: 'snaplet', // Usually your GitHub org/user name.
-  projectName: 'docs', // Usually your repo name.
-  plugins: ['docusaurus-plugin-segment'],
+  title: "Snaplet",
+  tagline: "Work with your database as easily as your code",
+  url: "https://docs.snaplet.dev",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.png",
+  organizationName: "snaplet", // Usually your GitHub org/user name.
+  projectName: "docs", // Usually your repo name.
+  plugins: ["docusaurus-plugin-segment"],
   themeConfig: {
+    algolia: {
+      appId: "PPVJZD9QQI",
+      apiKey: "221836c058f50b764becd187638295b2",
+      indexName: "snaplet-docs",
+    },
     segment: {
-      apiKey: 'VY4RrFl2Qu7bCRmxzQ7IIj9dK7MFMzrH',
+      apiKey: "VY4RrFl2Qu7bCRmxzQ7IIj9dK7MFMzrH",
     },
     colorMode: {
-      defaultMode: 'light',
+      defaultMode: "light",
       disableSwitch: true,
       respectPrefersColorScheme: false,
     },
     navbar: {
       logo: {
-        alt: 'Snaplet',
-        src: 'img/logo.svg',
+        alt: "Snaplet",
+        src: "img/logo.svg",
       },
       items: [
         {
-          label: 'Discord',
-          href: 'https://app.snaplet.dev/chat',
-          position: 'right',
+          label: "Discord",
+          href: "https://app.snaplet.dev/chat",
+          position: "right",
         },
         {
-          label: 'GitHub',
-          href: 'https://github.com/snaplet/docs',
-          position: 'right',
+          label: "GitHub",
+          href: "https://github.com/snaplet/docs",
+          position: "right",
         },
         {
-          href: 'https://app.snaplet.dev',
-          label: 'Login',
-          position: 'right',
+          href: "https://app.snaplet.dev",
+          label: "Login",
+          position: "right",
         },
       ],
     },
     footer: {
       links: [
         {
-          title: 'Community',
+          title: "Community",
           items: [
             {
-              label: 'Discord',
-              href: 'https://app.snaplet.dev/chat',
+              label: "Discord",
+              href: "https://app.snaplet.dev/chat",
             },
             {
-              label: 'Twitter',
-              href: 'https://twitter.com/_snaplet',
+              label: "Twitter",
+              href: "https://twitter.com/_snaplet",
             },
           ],
         },
         {
-          title: 'More',
+          title: "More",
           items: [
             {
-              label: 'Blog',
-              to: 'https://www.snaplet.dev/blog',
+              label: "Blog",
+              to: "https://www.snaplet.dev/blog",
             },
             {
-              label: 'GitHub',
-              href: 'https://github.com/snaplet',
+              label: "GitHub",
+              href: "https://github.com/snaplet",
             },
           ],
         },
@@ -79,24 +84,29 @@ module.exports = {
     prism: {
       theme: darkCodeTheme,
       darkTheme: darkCodeTheme,
-      additionalLanguages: ['bash'],
+      additionalLanguages: ["bash"],
     },
   },
   presets: [
     [
-      '@docusaurus/preset-classic',
+      "@docusaurus/preset-classic",
       {
         docs: {
-          routeBasePath: '/',
-          sidebarPath: require.resolve('./sidebars.js'),
-          editUrl:
-            'https://github.com/snaplet/docs/tree/main',
+          routeBasePath: "/",
+          sidebarPath: require.resolve("./sidebars.js"),
+          editUrl: "https://github.com/snaplet/docs/tree/main",
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       },
     ],
   ],
-  scripts: [{ src: 'https://plausible.io/js/script.js', defer: true, 'data-domain': 'snaplet.dev' }],
+  scripts: [
+    {
+      src: "https://plausible.io/js/script.js",
+      defer: true,
+      "data-domain": "snaplet.dev",
+    },
+  ],
 };
